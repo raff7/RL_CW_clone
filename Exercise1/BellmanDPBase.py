1ebc0e32284d096724c981e6c788781ee2a5f399
rff7 <lele.raffa97@gmIil.com>
2019-02-12 11:09:54 +0000

    EX1 completed

--- Exercise1/BellmanDPBase.py
+++ Exercise1/BellmanDPBase.py
@@ -3,17 +3,48 @@ from MDP import MDP
 class BellmanDPSolver(object):
 	def __init__(self):
 		self.MDP = MDP()
+		self.disc = 1
 
 	def initVs(self):
 
-	def BellmanUpdate(self):
+		self.VS = dict.fromkeys(self.MDP.S,0)
 
-		raise NotImplementedError
+	def BellmanUpdate(self):
+		policy =dict.fromkeys(self.MDP.S,[])
+		for s in self.MDP.S:
+			max_va = -100000000
+			for a in self.MDP.A:
+				p = self.MDP.probNextStates(s, a)
+				vs_a =0
+				for s1 in list(p.keys()):
+					vs_a += p[s1]*(self.MDP.getRewards(s,a,s1) + self.disc*self.VS[s1])
+				if (vs_a >= max_va):
+					if(vs_a==max_va):
+						policy[s].append(a)
+					else:
+						policy[s] = [a]
+					max_va = vs_a
+			self.VS[s] = max_va
+
+		return self.VS,policy
+
+
+
+
 		
 if __name__ == '__main__':
 	solution = BellmanDPSolver()
-	for i in range(20000):
+	solution.initVs()
+	for i in range(1):
 		values, policy = solution.BellmanUpdate()
-	print("Values : ", values)
-	print("Policy : ", policy)
+	#print("Values : ", values)
+	# for k,v in policy.items():
+	# 	print(k,v)
+	#print("Policy : ", policy)
+
+	for k, v in policy.items():
+		print(k,v)
+
+
+
 
